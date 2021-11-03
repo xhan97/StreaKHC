66a4b1e7a787d117b303d39f14fb750e8f40bb68
xhan97 <xhan@tulip.academy>
2021-11-04 00:22:01 +0800

    format shell

--- bin/run_grid_evaluation.sh
+++ bin/run_grid_evaluation.sh
@@ -19,14 +19,15 @@ set -exu
 output_dir=${1:-"exp_out"}
 TIME=$( (date +%Y-%m-%d-%H-%M-%S-%3N))
 output_dir="${output_dir}/$TIME"
-dataset_path=$STREASKH_ROOT/data/raw/
-dataset_runned_path=$STREASKH_ROOT/data/runned/
-shuffle_data_path=$STREASKH_ROOT/data/shuffle_data/
-shuffle_data_path="$shuffle_data_path/$TIME"
+shuffle_data_path="$STREASKH_DATA_SHUFFLE/$TIME"
 
 num_runs=10
-for suffix in '.csv'; do #'.tsv'
-    data_files=$(ls $dataset_path*$suffix)
+for suffix in '.csv' '.tsv'; do
+    if [ -z "$(ls $STREASKH_DATA*$suffix)" ]; then
+        echo "No dataset endwith ${suffix} in ${STREASKH_DATA}"
+        break
+    fi
+    data_files=$(ls $STREASKH_DATA*$suffix)
     for dataset_file in ${data_files}; do
         sh bin/util/shuffle_dataset.sh $dataset_file $num_runs $shuffle_data_path
         dataset_name=$(basename -s $suffix $dataset_file)
@@ -45,12 +46,12 @@ for suffix in '.csv'; do #'.tsv'
                         --beta ${beta} \
                         --psi 3 5 7 13 15 17 21 25 \
                         --train_size ${t_size}
-                    dot -Kdot -Tpng $exp_output_dir/tree.dot -o $exp_output_dir/tree.png
+                    #dot -Kdot -Tpng $exp_output_dir/tree.dot -o $exp_output_dir/tree.png
                 ) &
             done
             wait
         done
-        #mv $dataset_file $dataset_runned_path
+        #mv $dataset_file $STREASKH_DATA_RUNNED
     done
 done
 sh bin/util/collect_and_format_results.sh $output_dir
